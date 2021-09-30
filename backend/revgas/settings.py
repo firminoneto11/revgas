95e35a5dc784e7a829cc87e4e16d471ecf005e9d
Firmino Neto <firminoneto11@hotmail.com>
2021-09-30 20:36:12 -0300

    Changes for the deploy phase

--- backend/revgas/settings.py
+++ backend/revgas/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', cast=bool, default=False)
 
-ALLOWED_HOSTS = config('ALLOWED_HOSTS')
+ALLOWED_HOSTS = config('ALLOWED_HOSTS', default="revgas.herokuapp.com")
 
 
 # Application definition
@@ -143,7 +143,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = join(BASE_DIR, 'staticfiles')
+# STATIC_ROOT = join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
