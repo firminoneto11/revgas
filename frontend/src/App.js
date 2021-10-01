cb55a46610cd493aca3a421af472b4256494f641
firmino_neto <firminoneto11@hotmail.com>
2021-10-01 13:41:31 -0300

    stable version for frontend part finished

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,15 +6,17 @@ function App() {
     // declares hook to store fetch data
     const [data, setData] = useState()
 
-    // declares hook to store filtered element
-    const [filter, setFilter] = useState([])
-
-    // declares hook to store search input value
-    const [value, setValue] = useState('')
-
     // function to fetch api data
-    const getData = async () => {
+    const getData = async _ => {
         const url = 'https://revgas.herokuapp.com/revgas/api/banks/'
+
+        let response = await fetch(url)
+        response = await response.json()
+        setData(response)
+    }
+
+    const getSingleBankData = async comp_code => {
+        const url = `https://revgas.herokuapp.com/revgas/api/banks/${comp_code}`
 
         let response = await fetch(url)
         response = await response.json()
@@ -28,72 +30,79 @@ function App() {
 
     // main component
     const TableBody = props => {
+
         // all bank data as props
         const bankData = props.bankData
 
-        // filtered bank as props
-        const filterBank = props.filterBank
-
-        // maps the filtered element and stores it in a variable to render later
-        const filtered = filterBank && filterBank.map((el, index) => {
-            const id = el.id
-            const comp_code = el.compensation_code
-            const inst_name = el.institution_name
-
-            return (
-                <tr key={index}>
-                    <td>{id}</td>
-                    <td>{comp_code}</td>
-                    <td>{inst_name}</td>
-                </tr>
-            )
-        })
-
         // maps the data and stores it in a variable to render later
-        const banks = bankData && bankData.map((el, index) => {
-            const id = el.id
-            const comp_code = el.compensation_code
-            const inst_name = el.institution_name
+        let banks
+        if (bankData instanceof Array) {
+            banks = bankData && bankData.map((el, index) => {
+                const id = el.id
+                const comp_code = el.compensation_code
+                const inst_name = el.institution_name
 
-            return (
-                <tr key={index}>
-                    <td>{id}</td>
-                    <td>{comp_code}</td>
-                    <td>{inst_name}</td>
-                </tr>
-            )
-        })
+                return (
+                    <tr key={index}>
+                        <td>{id}</td>
+                        <td>{comp_code}</td>
+                        <td>{inst_name}</td>
+                    </tr>
+                )
+            })
+        }
+        // else if (bankData && 'invalid_bank' in Object.entries(bankData)) {
 
-        // conditional render to control rather the whole data is shown or only the filtered element
-        if (filter.length === 0) {
-            return <tbody>{banks}</tbody>
-        } else {
-            return <tbody>{filtered}</tbody>
+        // }
+        else {
+            if (bankData) {
+                const id = bankData.id
+                const comp_code = bankData.compensation_code
+                const inst_name = bankData.institution_name
+
+                banks = (
+                    <tr key="1">
+                        <td>{id}</td>
+                        <td>{comp_code}</td>
+                        <td>{inst_name}</td>
+                    </tr>
+                )
+            }
+        }
+
+        return <tbody>{banks}</tbody>
+    }
+
+    function getBank(event) {
+        const comp_code = event.target.previousSibling.value
+        if (comp_code.length > 0) {
+            getSingleBankData(comp_code)
+        }
+        else {
+            getData()
         }
     }
 
-    // finds current element based on current input value
-    function getCurrentBank(event) {
-        const currentValue = event.target.value
-        setValue(currentValue)
-
-        if (value === '') {
-            setFilter([])
-        } else {
-            // important to use filter so it returns multiple results
-            const currentElement = data.filter(el => el.institution_name.toLowerCase().includes(value.toLocaleLowerCase()))
-            setFilter(currentElement)
+    function search(event) {
+        if (event.keyCode === 13) {
+            event.preventDefault()
+            event.target.nextSibling.click()
         }
     }
 
     // Final Front end
     return (
-        <div className="body">
+        <div>
+
             <h1 className="title">Banks Index</h1>
+
             <div className="search-box">
-                <p className="search-text">Search a bank by name:</p>
-                <input className="search-input" value={value} onChange={getCurrentBank} />
+                <p className="search-text">Search a bank by the Compensation Code:</p>
+                <input type="number" className="search-input" onKeyUp={search} />
+                <button className="look-for-bank" onClick={getBank}>Search</button>
             </div>
+
+            <div className="table-container">
             <table className="table">
                 <thead>
                     <tr>
@@ -102,8 +111,10 @@ function App() {
                         <th>Institution name</th>
                     </tr>
                 </thead>
-                <TableBody filterBank={filter} bankData={data} />
+                <TableBody bankData={data} />
             </table>
+            </div>
+
         </div>
     );
 }
